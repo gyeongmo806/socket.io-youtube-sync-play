eb47487d19f8549d92c875490ffba3bc131e3726
gyeongmo_806 <jip080620@gmail.com>
2020-04-23 09:12:20 +0900

    socketio youtube sync play

--- app.js
+++ app.js
@@ -1,11 +1,32 @@
-const express = require('express')
-const app = express()
-const io = require('socket.io')
+var app = require('express')();
+var http = require('http').createServer(app);
+var io = require('socket.io')(http);
 
 app.get('/', (req,res) => {
     res.sendFile(__dirname+'/index.html')
 })
+let hostId
+io.on('connection', (socket) => {
+    if(hostId == undefined){
+        hostId = socket.id
+        console.log(hostId)
+    }
+    socket.on('time' ,(data) => {
+        console.log(data)
+        console.log(hostId, socket.id)
+        console.log(socket.id+":"+data.time)
+        if(socket.id == hostId){
+            socket.broadcast.emit('res', data.time)
+            setTimeout(()=>{
+                io.emit('play',true)
+            },100)
+            
+        }
+    })
+    
+});
 
-app.listen(3000, () => {
+http.listen(3000, () => {
     console.log('listening on :3000')
 })
+
